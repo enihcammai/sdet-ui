d873c40f217029986c099fe41241fc327bb9e8a2
enihcammai <mikhailov.val.ssf@gmail.com>
2025-12-27 21:54:50 +0400

    Fixing imports

--- src/test/java/com/simbirsoft/ui/pages/BasePage.java
+++ src/test/java/com/simbirsoft/ui/pages/BasePage.java
@@ -3,7 +3,6 @@ package com.simbirsoft.ui.pages;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.devtools.DevTools;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
